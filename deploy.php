bf936fca2ced04fc9307a496c0e7ce0df67b35b2
Amelia Gianoli <gianolia@students.westerntc.edu>
2026-01-18 15:47:31 -0600

    added .cpanel.yml file, which should enable push deployment

--- deploy.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Path to your repository
-$repo_path = 'C:\Users\board\OneDrive - Western Wisconsin Technical College\3 Spring 2026\2 Web Programming II\web2';
-// Pull latest changes
-exec("cd {$repo_path} && git pull origin main", $output);
-// Log output (optional)
-file_put_contents('deploy.log', implode("\n", $output));
-echo "Deployment Successful!";
-?>
